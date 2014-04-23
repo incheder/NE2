dea5fbed650ca9def7640df5204b3e750ede9f96
Eder Perez <edpefal@gmail.com>
2014-04-23 15:48:02 -0500

    vista descripcion del show

--- src/com/ninetoseven/series/activitys/ShowDescriptionActivity.java
+++ src/com/ninetoseven/series/activitys/ShowDescriptionActivity.java
@@ -11,6 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.ninetoseven.series.R;
 
@@ -53,6 +54,7 @@ public class ShowDescriptionActivity extends ActionBarActivity {
 	public static class PlaceholderFragment extends Fragment {
 
 		private ImageView ivShow;
+		private TextView tvSeason,tvStart,tvStatus,tvDuration,tvNetwork,tvTime,tvSummary;
 		Random r = new Random();
 		public PlaceholderFragment() {
 		}
@@ -70,6 +72,23 @@ public class ShowDescriptionActivity extends ActionBarActivity {
 			View rootView = inflater.inflate(R.layout.fragment_show_descripcion, container,
 					false);
 			ivShow = (ImageView)rootView.findViewById(R.id.ivShowDescriptionImage);
+			tvDuration = (TextView)rootView.findViewById(R.id.tvDuration);
+			tvNetwork = (TextView)rootView.findViewById(R.id.tvNetwork);
+			tvSeason = (TextView)rootView.findViewById(R.id.tvSeason);
+			tvStart = (TextView)rootView.findViewById(R.id.tvStartDate);
+			tvStatus = (TextView)rootView.findViewById(R.id.tvStatus);
+			tvSummary = (TextView)rootView.findViewById(R.id.tvSummary);
+			tvTime = (TextView)rootView.findViewById(R.id.tvTime);
+			
+			tvDuration.setText("60 min");
+			tvNetwork.setText("ABC");
+			tvSeason.setText("5");
+			tvStart.setText("22-05-2013");
+			tvStatus.setText("live");
+			tvSummary.setText("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,");
+			tvTime.setText("Thursday at 10:00 pm");
+			
+			
 			if(r.nextInt(2)==1)
 			{
 				ivShow.setImageResource(R.drawable.show);
